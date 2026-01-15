b5f74b13cf97f22cea3f700e581694f475ef4433
buke <1013738+buke@users.noreply.github.com>
2026-01-15 02:06:14 +0000

    chore(sync): mirror internal packages into pkg/ (auto)

---
+++ pkg/fourslash/tests/gen/completionsOverridingProperties3_test.go
@@ -0,0 +1,38 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/buke/typescript-go-internal/pkg/fourslash"
+	. "github.com/buke/typescript-go-internal/pkg/fourslash/tests/util"
+	"github.com/buke/typescript-go-internal/pkg/lsp/lsproto"
+	"github.com/buke/typescript-go-internal/pkg/testutil"
+)
+
+func TestCompletionsOverridingProperties3(t *testing.T) {
+	fourslash.SkipIfFailing(t)
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = `interface I {
+    prop: string;
+}
+class C implements I {
+    public pr/**/: string | number;
+}`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.VerifyCompletions(t, "", &fourslash.CompletionsExpectedList{
+		IsIncomplete: false,
+		ItemDefaults: &fourslash.CompletionsExpectedItemDefaults{
+			CommitCharacters: &[]string{},
+			EditRange:        Ignored,
+		},
+		Items: &fourslash.CompletionsExpectedItems{
+			Includes: []fourslash.CompletionsExpectedItem{
+				&lsproto.CompletionItem{
+					Label: "prop",
+				},
+			},
+		},
+	})
+}
