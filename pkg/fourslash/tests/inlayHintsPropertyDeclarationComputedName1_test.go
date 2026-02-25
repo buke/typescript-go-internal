3a0c4d890f71215a9ea50e5302ac20e5c61b23fa
buke <1013738+buke@users.noreply.github.com>
2026-02-25 08:35:51 +0000

    chore(sync): mirror internal packages into pkg/ (auto)

---
+++ pkg/fourslash/tests/inlayHintsPropertyDeclarationComputedName1_test.go
@@ -0,0 +1,28 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/buke/typescript-go-internal/pkg/fourslash"
+	"github.com/buke/typescript-go-internal/pkg/ls/lsutil"
+	"github.com/buke/typescript-go-internal/pkg/testutil"
+)
+
+func TestInlayHintsPropertyDeclarationComputedName1(t *testing.T) {
+	t.Parallel()
+
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = `function foo() {
+  const sym = Symbol();
+  class C {
+    [sym] = 123;
+  }
+}`
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.VerifyBaselineInlayHints(t, nil /*span*/, &lsutil.UserPreferences{
+		InlayHints: lsutil.InlayHintsPreferences{
+			IncludeInlayPropertyDeclarationTypeHints: true,
+		},
+	})
+}
