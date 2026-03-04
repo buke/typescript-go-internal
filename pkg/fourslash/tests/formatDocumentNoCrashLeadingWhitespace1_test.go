e515e659bfe5e4db58d665abd0aa592a99f0a032
buke <1013738+buke@users.noreply.github.com>
2026-03-04 02:59:27 +0000

    chore(sync): mirror internal packages into pkg/ (auto)

---
+++ pkg/fourslash/tests/formatDocumentNoCrashLeadingWhitespace1_test.go
@@ -0,0 +1,17 @@
+package fourslash_test
+
+import (
+	"testing"
+
+	"github.com/buke/typescript-go-internal/pkg/fourslash"
+	"github.com/buke/typescript-go-internal/pkg/testutil"
+)
+
+func TestFormatDocumentNoCrashLeadingWhitespace1(t *testing.T) {
+	t.Parallel()
+	defer testutil.RecoverAndFail(t, "Panic on fourslash test")
+	const content = " \n"
+	f, done := fourslash.NewFourslash(t, nil /*capabilities*/, content)
+	defer done()
+	f.FormatDocument(t, "")
+}
